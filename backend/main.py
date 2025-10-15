71c7c4c6a760506858a2efd81221e4d4bac27fde
3voronkov <226914229+3voronkov@users.noreply.github.com>
2025-10-15 15:10:10 +0300

    бэкенд

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.request import Request
+from sanic.response import HTTPResponse, text
+import os
 
 app = Sanic("MetroChatBot")
 
+@app.main_process_start
+async def main_process_start(*_):
+    app.ctx.gigachat_key = os.getenv("GIGACHAT_KEY")
+
 @app.post("/api/chat")
-async def index(req):
+async def index(request: Request) -> HTTPResponse:
     return text("Hello world")
+
+if __name__ == "__main__":
+    app.run(os.getenv("HOST"), int(os.getenv("PORT")))
