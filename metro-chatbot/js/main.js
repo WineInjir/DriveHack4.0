f557b85a3f1754c5fcc70f22c1d9562911b482e0
WineInjir <gdisk1002@gmail.com>
2025-10-15 16:37:31 +0300

    123

--- metro-chatbot/js/main.js
+++ metro-chatbot/js/main.js
@@ -76,7 +76,7 @@ async function sendMessage() {
   input.value = "";
 
   try {
-    const response = await fetch("/api/chat", {
+    const response = await fetch("http://127.0.0.1/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
