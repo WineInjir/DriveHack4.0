a92ca7b6bf991d69b9131c960bf5a94698820081
3voronkov <226914229+3voronkov@users.noreply.github.com>
2025-10-15 16:40:37 +0300

    backend test

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import HTTPResponse, text
-import os, aiohttp, json
+import os, aiohttp, json, pathlib
 
 app = Sanic("MetroChatBot")
+
+STATIC_DIR = (pathlib.Path(__file__) / "../../metro-chatbot").resolve()
+print(f"Static: {STATIC_DIR}")
+app.static("/", str(STATIC_DIR), name="index_static")
 
 @app.post("/api/chat")
 async def index(request: Request) -> HTTPResponse:
