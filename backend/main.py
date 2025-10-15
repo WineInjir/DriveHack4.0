0207e5f4a014ff697d59028eb139d377b286a268
3voronkov <226914229+3voronkov@users.noreply.github.com>
2025-10-15 16:36:04 +0300

    backend test

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import HTTPResponse, text
-import os, aiohttp, gigachat
+import os, aiohttp, json
 
 app = Sanic("MetroChatBot")
 
 @app.post("/api/chat")
 async def index(request: Request) -> HTTPResponse:
-    pass
+    data = request.json
+    print(json.dumps(data))
 
 if __name__ == "__main__":
-    app.run(os.getenv("HOST"), int(os.getenv("PORT")))
+    app.run("127.0.0.2", 8080)
