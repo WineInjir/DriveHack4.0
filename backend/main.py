cc8d0c3f2e058bb8b00926ffe6bcea6a796d8989
3voronkov <226914229+3voronkov@users.noreply.github.com>
2025-10-15 16:31:04 +0300

    хз

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,13 @@
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import HTTPResponse, text
-import os, aiohttp
-from gigachat import GigaChat
+import os, aiohttp, gigachat
 
 app = Sanic("MetroChatBot")
 
-
-@app.main_process_start
-async def main_process_start(*_):
-    app.ctx.gigachat_token = GigaChat(credentials="MDE5OWU3NDUtOTYzMC03OTA4LWFmODUtMTBhZmVmYWJhMWY5OmQxMjMzOTdiLTllZDQtNDIwNS1iMDQwLTljZjk0OGE1YzlkMw==",)
-    app.ctx.gigachat_request_token = app.ctx.gigachat_token.get_token()
-
 @app.post("/api/chat")
 async def index(request: Request) -> HTTPResponse:
-    async with aiohttp.request(
-        method="POST",
-        url=request.app.ctx.gigachat_endpoint,
-        json={
-            "model": "GigaChat",
-            "messages": [],
-            "temperature": 0.7,
-        },
-    ):
-        pass
+    pass
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", 8080)
+    app.run(os.getenv("HOST"), int(os.getenv("PORT")))
