afa8094b5bcfcc24b1fe1e641641bb0c73aa458a
3voronkov <226914229+3voronkov@users.noreply.github.com>
2025-10-15 15:25:12 +0300

    бэкенд 3

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import HTTPResponse, text
-import os
+import os, aiohttp
 
 app = Sanic("MetroChatBot")
 
@@ -10,11 +10,18 @@ async def main_process_start(*_):
     # константы
     app.ctx.gigachat_endpoint = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
 
-    app.ctx.gigachat_key = os.getenv("GIGACHAT_KEY")
+    # переменные среды
+    with open("token.txt", "r") as f:
+        app.ctx.gigachat_key = f.read().decode()
 
 @app.post("/api/chat")
 async def index(request: Request) -> HTTPResponse:
-    return text("Hello world")
+    async with aiohttp.request(
+        method="POST",
+        url=request.app.ctx.gigachat_endpoint,
+        json={"sosal": True},
+    ):
+        pass
 
 if __name__ == "__main__":
     app.run(os.getenv("HOST"), int(os.getenv("PORT")))
